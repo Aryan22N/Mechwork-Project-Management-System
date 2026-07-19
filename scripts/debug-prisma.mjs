9ca32b8b45b54faf517d53069d5e9a8cb4af8be2
Aryan Nandanwar <aryannandanwar738@gmail.com>
2026-07-20 01:22:05 +0530

    feat: implement  management system with location-based attendance tracking

--- scripts/debug-prisma.mjs
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import { prisma } from "../lib/db.js";
-
-async function debug() {
-    console.log("=== Prisma Client Keys ===");
-    console.log(Object.keys(prisma).filter(k => !k.startsWith("_")));
-}
-
-debug().catch(console.error);
